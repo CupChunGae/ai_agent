65eee402a33366e8ce99a14031a817b66d185741
CupChunGae <c_nim@hotmail.com>
2025-12-09 15:56:40 +0000

    added calculator functions

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ if api_key == None:
 client = genai.Client(api_key = api_key)
 def main():
     parser = argparse.ArgumentParser(description="Gemini")
-    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role = "user", parts = [types.Part(text = args.user_prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
